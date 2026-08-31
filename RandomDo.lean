719d74f489e69d2ef9aa3f9556b5e59a950b9a65
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-08-31 17:13:52 +0200

    Update RandomDo.lean

--- RandomDo.lean
+++ RandomDo.lean
@@ -2,19 +2,12 @@ module  -- shake: keep-all --deprecated_module: ignore
 
 public import RandomDo.ForMathlib.MeasureTheory.MeasurableSpace.Embedding
 public import RandomDo.Measurable
-public import RandomDo.Monad.Examples
 public import RandomDo.Monad.ForInInstances
 public import RandomDo.Monad.Instances
 public import RandomDo.Monad.MeasurableSpace
 public import RandomDo.Monad.Notation
-public import RandomDo.Probability.AlgTrace
-public import RandomDo.Probability.Examples
-public import RandomDo.Probability.Record
-public import RandomDo.Probability.Tactic
-public import RandomDo.Probability.Thompson
-public import RandomDo.Probability.Trace
+public import RandomDo.Tactic.Deriving
 public import RandomDo.Tactic.Elab
-public import RandomDo.Tactic.Examples
 public import RandomDo.Tactic.ForInStep
 public import RandomDo.Tactic.IsMarkov
 public import RandomDo.Tactic.Lemmas
