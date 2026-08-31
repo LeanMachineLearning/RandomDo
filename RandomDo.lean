c876e383931797acd136126e6d01a5a668458d06
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-08-31 17:14:24 +0200

    Update RandomDo.lean

--- RandomDo.lean
+++ RandomDo.lean
@@ -6,6 +6,12 @@ public import RandomDo.Monad.ForInInstances
 public import RandomDo.Monad.Instances
 public import RandomDo.Monad.MeasurableSpace
 public import RandomDo.Monad.Notation
+public import RandomDo.Probability.AlgTrace
+public import RandomDo.Probability.Examples
+public import RandomDo.Probability.Record
+public import RandomDo.Probability.Tactic
+public import RandomDo.Probability.Thompson
+public import RandomDo.Probability.Trace
 public import RandomDo.Tactic.Deriving
 public import RandomDo.Tactic.Elab
 public import RandomDo.Tactic.ForInStep
