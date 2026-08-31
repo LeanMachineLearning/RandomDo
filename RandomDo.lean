6b0b58e5483eaa8f3ab7046524c9c938b842dd13
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-08-31 16:10:59 +0200

    Remove examples

--- RandomDo.lean
+++ RandomDo.lean
@@ -2,14 +2,12 @@ module  -- shake: keep-all --deprecated_module: ignore
 
 public import RandomDo.ForMathlib.MeasureTheory.MeasurableSpace.Embedding
 public import RandomDo.Measurable
-public import RandomDo.Monad.Examples
 public import RandomDo.Monad.ForInInstances
 public import RandomDo.Monad.Instances
 public import RandomDo.Monad.MeasurableSpace
 public import RandomDo.Monad.Notation
 public import RandomDo.Tactic.Deriving
 public import RandomDo.Tactic.Elab
-public import RandomDo.Tactic.Examples
 public import RandomDo.Tactic.ForInStep
 public import RandomDo.Tactic.IsMarkov
 public import RandomDo.Tactic.Lemmas
